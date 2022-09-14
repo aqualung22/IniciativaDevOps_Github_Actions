d8c764b2b32bdfd75d5765ba1fb680dc2276c8ad
Guilherme Ferreira <aqualung22@gmail.com>
2022-09-14 14:19:27 -0300

    diversos

--- iac/main.tf
+++ iac/main.tf
@@ -12,7 +12,7 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_kubernetes_cluster" "k8s_iniciativa" {
+resource "digitalocean_kubernetes_cluster" "k8s_iniciativa_devops" {
   name = var.k8s_name
   region = var.region
   # Grab the latest version slug from `doctl kubernetes options versions`
@@ -26,10 +26,10 @@ resource "digitalocean_kubernetes_cluster" "k8s_iniciativa" {
 }
 
 resource "digitalocean_kubernetes_node_pool" "node_premium" {
-  cluster_id = digitalocean_kubernetes_cluster.k8s_iniciativa.id
+  cluster_id = digitalocean_kubernetes_cluster.k8s_iniciativa_devops.id
   name       = "premium"
   size       = "s-4vcpu-8gb"
-  node_count = 2
+  node_count = 3
 }
 
 variable "do_token" {}
@@ -37,10 +37,10 @@ variable "k8s_name" {}
 variable "region" {}
 
 output "kube_endpoint" {
-  value = digitalocean_kubernetes_cluster.k8s_iniciativa.endpoint
+  value = digitalocean_kubernetes_cluster.k8s_iniciativa_devops.endpoint
 }
 
 resource "local_file" "kube_config" {
-    content  = digitalocean_kubernetes_cluster.k8s_iniciativa.kube_config.0.raw_config
+    content  = digitalocean_kubernetes_cluster.k8s_iniciativa_devops.kube_config.0.raw_config
     filename = "kube_config.yaml"
 }
